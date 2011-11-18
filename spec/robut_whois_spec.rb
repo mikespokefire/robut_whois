7b1c727d9e563909fc1b0a661acc2ddc07200c0d
Michael Smith <mike@spokefire.co.uk>
2011-11-18 14:38:49 +0000

    Better matchers

--- spec/robut_whois_spec.rb
+++ spec/robut_whois_spec.rb
@@ -8,11 +8,11 @@ describe RobutWhois do
   end
   
   it "should be an instance of Robut::Whois::Plugin" do
-    @plugin.should be_a(RobutWhois::Plugin)
+    @plugin.should be_an_instance_of(RobutWhois::Plugin)
   end
   
   it "should include Robut::Plugin" do
-    @plugin.should be_a(Robut::Plugin)
+    @plugin.should be_a_kind_of(Robut::Plugin)
   end
   
 end
