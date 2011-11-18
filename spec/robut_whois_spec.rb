b51df837226b7766ee5b7f83c42373e3afb410b1
Michael Smith <mike@spokefire.co.uk>
2011-11-18 14:41:15 +0000

    Use let instead of instance variables

--- spec/robut_whois_spec.rb
+++ spec/robut_whois_spec.rb
@@ -2,17 +2,15 @@ require 'spec_helper'
 
 describe RobutWhois do
   
-  before(:each) do
-    @connection = Robut::ConnectionMock.new
-    @plugin = RobutWhois::Plugin.new(@connection)
-  end
+  let(:connection) { Robut::ConnectionMock.new }
+  let(:plugin) { RobutWhois::Plugin.new(connection) }
   
   it "should be an instance of Robut::Whois::Plugin" do
-    @plugin.should be_an_instance_of(RobutWhois::Plugin)
+    plugin.should be_an_instance_of(RobutWhois::Plugin)
   end
   
   it "should include Robut::Plugin" do
-    @plugin.should be_a_kind_of(Robut::Plugin)
+    plugin.should be_a_kind_of(Robut::Plugin)
   end
   
 end
